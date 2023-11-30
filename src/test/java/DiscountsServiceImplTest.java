c110106e46799e1237a87fb2a175c0c39436e2f7
Zi Di <40129612+ZiDiZhu@users.noreply.github.com>
2023-11-30 13:41:08 -0500

    updated discount unit test (#18)
    * discounts page
    
    * Fixed: Discount / Used Products now show when logged in
    
    * Added Unit Test for Discounts
    
    * Discount Page
    
    * deleted assertion
    
    * deleted discount price member in product
    
    * (deleting discount setter in productbean
    
    * bug fix
    
    * Removed discount generation
    
    * Remove .idea folder
    
    * Update DiscountsServiceImplTest.java
    
    * product bean delete onsale bool
    
    * fix stuff
    
    * remove wrong changes and fix tests
    
    ---------
    
    Co-authored-by: Yu Qiao Chen <alaire3@gmail.com>
    Co-authored-by: matias <matiasbeniluz@gmail.com>

---
+++ src/test/java/DiscountsServiceImplTest.java
@@ -0,0 +1,93 @@
+package test.java;
+
+import com.shashi.beans.DiscountBean;
+import com.shashi.service.impl.DiscountServiceImpl;
+import com.shashi.utility.DBUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscountServiceImplTest {
+    static Connection con;
+
+    private Savepoint savepoint;
+
+    private DiscountServiceImpl discountService;
+
+    private List<DiscountBean> discounts= new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        con = DBUtil.provideConnection();
+
+        try {
+            con.setAutoCommit(false);
+            savepoint = con.setSavepoint();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        discountService = new DiscountServiceImpl();
+
+
+        for (int i = 0; i <= 3; i++) {
+            String discountId = String.valueOf(i);
+            String discountName = "Discount " + i;
+            int discountPercentage = (i * 5) + 5;  // Generating a percentage based on the index
+            LocalDate startDate = LocalDate.now().plusDays(i * 10);
+            LocalDate endDate = LocalDate.now().plusDays((i * 10) + 30);
+
+            DiscountBean discount = new DiscountBean(discountId, discountName, discountPercentage, startDate, endDate);
+            discounts.add(discount);
+            discountService.addDiscount(discount);
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        try {
+            con.rollback(savepoint);
+            con.setAutoCommit(true);
+            DBUtil.closeConnection(con);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    void getDiscountById_ShouldReturnNullForNonExistentDiscount() {
+        DiscountBean result = discountService.getDiscountDetails("999");
+        assertNull(result);
+    }
+
+    @Test
+    void updateDiscount_ShouldUpdateDiscount() {
+        DiscountBean discountToUpdate = discountService.getDiscountDetails("1");
+        discountToUpdate.setDiscountPercentage(20);
+
+        discountService.updateDiscountIntoDB("1",discountToUpdate);
+
+
+        DiscountBean updatedDiscount = discountService.getDiscountDetails("1");
+        assertNotNull(updatedDiscount);
+        assertEquals(20, updatedDiscount.getDiscountPercentage());
+    }
+
+    @Test
+    void deleteDiscount_ShouldDeleteDiscount() {
+        discountService.removeDiscount("1");
+        DiscountBean deletedDiscount = discountService.getDiscountDetails("1");
+
+        assertNull(deletedDiscount);
+    }
+}
